4f8c1f7a5b9887db4374f449ff4d37618f339c4b
jerald6111 <60886695+jerald6111@users.noreply.github.com>
2021-10-06 21:39:41 -0400

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -310,7 +310,7 @@ class General(commands.Cog, name="general"):
                     embed.add_field(name='💔Losses', value=r['lose_total'])
                     embed.add_field(name='🛡️Draws', value=r['draw_total'])
                     embed.add_field(name='⚗Daily SLP',
-                                    value=str(self.get_unclaimed_slp(member_or_ronin_address)-self.get_daily_slp(member_or_ronin_address.replace('ronin:', '0x'))))
+                                    value=str(self.get_total_slp(member_or_ronin_address)-self.get_daily_slp(member_or_ronin_address.replace('ronin:', '0x'))))
                     embed.add_field(name='⚗SLP Current Balance', value=r['in_game_slp'])
                     embed.add_field(name='⚗Total SLP', value=r['total_slp'])
                     if scholar_percentage:
@@ -327,7 +327,7 @@ class General(commands.Cog, name="general"):
                                     value=datetime.fromtimestamp(int(f"{r['next_claim']}")).strftime(
                                         '%Y-%m-%d %H:%M:%S'))
                     if scholar_percentage:
-                        php, usd = self.convert_slp(scholar_percentage)
+                        php, usd = self.convert_slp(get_unclaimed_slp * scholar_percentage) 
                         embed.add_field(name='₱PHP Conversion', value=str(php))
                         embed.add_field(name='USD Conversion', value=str(usd))
                     else:
